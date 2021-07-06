f6e4a43afbcbe4453c38bb5f741a2db7efea325c
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2021-07-06 14:30:42 +0530

    Add files via upload

--- rope.js
+++ rope.js
@@ -14,9 +14,16 @@ class Rope{
              World.add(world,this.rope)
     }
     
-  display(){
-strokeWeight(2)
-line(body1.x,body1.y,body2.x,body2.y)
-  }
+ 
+    display() { 
+      var pointA=this.rope.bodyA.position;
+       var pointB=this.rope.bodyB.position; 
+       strokeWeight(2); var Anchor1X=pointA.x 
+       var Anchor1Y=pointA.y 
+       var Anchor2X=pointB.x+this.offsetX 
+       var  Anchor2Y=pointB.y+this.offsetY 
+        line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y); }
+
+ 
   
 }
